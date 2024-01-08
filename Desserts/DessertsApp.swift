0178ab1e35ba1634900a034e5c4510aa941dff14
JD Paul <jdpaul123@gmail.com>
2024-01-08 10:05:53 -0800

    Initial Commit

---
+++ Desserts/DessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertsApp.swift
+//  Desserts
+//
+//  Created by Jonathan Paul on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
