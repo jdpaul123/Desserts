3ff7a0202deddbf8fe1cdc9d408509692a30f457
JD Paul <jdpaul123@gmail.com>
2024-01-09 11:55:20 -0800

    Loading Screen now animates the app icon rather than using a ProgressView.

--- Desserts/ViewsAndViewModels/LoadingScreen.swift
+++ Desserts/ViewsAndViewModels/LoadingScreen.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct LoadingScreen: View {
+    @State private var isRotating = 0.0
+
     var body: some View {
         VStack(spacing: 20) {
             Image(.iceCreamDog)
                 .resizable()
                 .frame(width: 150, height: 150)
                 .cornerRadius(10)
-            ProgressView()
+                .rotationEffect(.degrees(isRotating))
+                .onAppear {
+                    withAnimation(.linear(duration: 1)
+                        .speed(0.1).repeatForever(autoreverses: false)) {
+                            isRotating = 360.0
+                        }
+                }
+                .padding()
             Text("Loading...")
         }
     }
