ba7a37eacd7d616d83029e63509684271ad3b057
JD Paul <jdpaul123@gmail.com>
2024-01-09 11:45:18 -0800

    Dog is a part of the Loading Screen now

--- Desserts/ViewsAndViewModels/LoadingScreen.swift
+++ Desserts/ViewsAndViewModels/LoadingScreen.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct LoadingScreen: View {
     var body: some View {
         VStack(spacing: 20) {
+            Image(.iceCreamDog)
+                .resizable()
+                .frame(width: 150, height: 150)
+                .cornerRadius(10)
             ProgressView()
             Text("Loading...")
         }
